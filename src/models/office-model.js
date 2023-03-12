8111de15847efbbb020d9d733fbe9b3161f08845
Daniil <daniil.kobylkin.pro@gmail.com>
2023-03-12 22:33:29 +0300

    office model

---
+++ src/models/office-model.js
@@ -0,0 +1,10 @@
+import { Schema, model } from 'mongoose';
+
+const UserSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: false },
+    phone: { type: String, required: false },
+    address: { type: String, required: true }
+})
+
+export default model('Office', UserSchema, 'offices');
