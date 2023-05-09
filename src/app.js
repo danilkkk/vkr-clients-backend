dd2cb872c3ffcac7e21ac405ac87410f0c4c7aab
Daniil <daniil.kobylkin.pro@gmail.com>
2023-05-09 14:32:37 +0300

    prepared for tests

---
+++ src/app.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import authRouter from './routers/auth-router.js';
+import errorMiddleware from "./middlewares/error-middleware.js";
+import rolesRouter from "./routers/roles-router.js";
+import usersRouter from "./routers/users-router.js";
+import officesRouter from "./routers/offices-router.js";
+import servicesRouter from "./routers/services-router.js";
+import schedulePatternsRouter from "./routers/schedule-patterns-router.js";
+import scheduleRouter from "./routers/schedule-router.js";
+import recordsRouter from "./routers/records-router.js";
+
+const app = express()
+    .use(express.json())
+    .use(cookieParser())
+    .use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    })
+    .use('/auth', authRouter)
+    .use('/users', usersRouter)
+    .use('/roles', rolesRouter)
+    .use('/offices', officesRouter)
+    .use('/services', servicesRouter)
+    .use('/patterns', schedulePatternsRouter)
+    .use('/schedule', scheduleRouter)
+    .use('/records', recordsRouter)
+    .use(errorMiddleware);
+
+app.response.getCurrentUser = function () {
+    console.log('getCurrentUser');
+    console.log(this.locals.currentUser);
+    return this.locals.currentUser;
+}
+
+export default app;
---
+++ src/server.js
@@ -0,0 +1,66 @@
+import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import logger from "./logger.js";
+import telegramBotService from "./services/telegram-bot-service.js";
+import startNotify from "./services/notifier-service.js";
+import app from './app.js'
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5005;
+const DATABASE_URI = /*process.env.DB_URL ||*/ `mongodb://localhost:27017`;
+const DB_NAME = process.env.DB_NAME || 'clients';
+
+let server;
+
+const startServer = async () => {
+    const start = Date.now();
+
+    try {
+        logger.info('Starting server...');
+
+        await mongoose.connect(DATABASE_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            dbName: DB_NAME,
+        }, (e) => {
+            if (e) {
+                logger.error(e);
+            } else {
+                logger.info('Database is connected')
+            }
+        });
+
+        server = app.listen(PORT, () => {
+            logger.info(`Server was started on port ${PORT} in ${(Date.now() - start).toFixed(2)} ms`);
+            startNotify();
+            logger.info(`start notifying...`);
+        });
+    } catch (e) {
+        logger.error(e);
+    }
+}
+
+startServer();
+
+const cleanUp = () => {
+    logger.info('Stopping the server...');
+
+    telegramBotService.stopPoling().then(() => {
+        logger.info('telegramBotService is closed.');
+    });
+
+    if (server) {
+        server.close(() => {
+            logger.info('Server is stopped.');
+
+            mongoose.connection.close(false, () => {
+                logger.info('Connection to MongoDB is closed.');
+                process.exit(0);
+            });
+        });
+    }
+}
+
+process.on('SIGTERM', cleanUp);
+process.on('SIGINT', cleanUp);
